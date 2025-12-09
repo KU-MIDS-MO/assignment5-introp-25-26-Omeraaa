e789fd8eedcab6fb3148c8231b5798391c329c47
Ömer Boruç <omerboruc@Omer-MacBook-Pro.local>
2025-12-09 02:10:14 +0100

    Done

--- approx_real_root.py
+++ approx_real_root.py
@@ -1,4 +1,24 @@
 def approx_real_root(coeffs, interval):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    a = interval[0]
+    b = interval[1]
+    
+    for i in range(1000):
+        mid = (a + b) / 2.0
+        
+        val_mid = 0
+        for k in range(len(coeffs)):
+            val_mid = val_mid + coeffs[k] * (mid ** k)
+            
+        if abs(val_mid) < 0.000000001:
+            return mid
+            
+        val_a = 0
+        for k in range(len(coeffs)):
+            val_a = val_a + coeffs[k] * (a ** k)
+            
+        if val_a * val_mid < 0:
+            b = mid
+        else:
+            a = mid
+            
+    return mid
